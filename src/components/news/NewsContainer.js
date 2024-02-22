82e94f8d5cb0dab09671ceddc1214e998da30a55
vik-ashsaini <vikashsaini879@gmail.com>
2024-02-22 07:32:39 +0530

    NewsItem changes

--- src/components/news/NewsContainer.js
+++ src/components/news/NewsContainer.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 import NewsItem from './NewsItem';
+import { wait } from '@testing-library/user-event/dist/utils';
 
 export class NewsContainer extends Component {
+  
+  constructor(){
+    super();
+    this.state={
+      articles:[],
+      loading:false
+    }
+  }
+
+  async componentDidMount()
+  {
+    let url='https://newsapi.org/v2/top-headlines?country=us&apiKey=dbe57b028aeb41e285a226a94865f7a7';
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({articles:parsedData.articles});
+    console.log(this.parsedData.articles);
+  }
+
   render() {
     return (
-      <div>
-        NewsContainer
-        <NewsItem />
+      <div className='container my-3'>
+        <h2>News Monkey - Top Headlines</h2>
+        <div className='row'>
+        {this.state.articles.map((e)=>{
+          return <div className='col=md-4' key={e.url}>
+            <NewsItem title={e.title?e.title.slice(0,45):''} description={e.description?e.description.slice(0,88):''}
+            imageUrl={e.urlToImage} newsUrl={e.url} />
+          </div>
+        })}
+        </div>
       </div>
     )
   }
--- src/components/news/NewsItem.js
+++ src/components/news/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title,description,imageUrl,newsUrl} =this.props;
     return (
-      <div>
-        News Item
+      <div className='my-3'>
+        <div className='card'>
+          <img className='card-img-top' src={!imageUrl?imageUrl:''} alt=''/>
+          <div className='card-body'>
+            <h5 className='card-title'>{title}...</h5>
+            <p className='card-text'>{description}...</p>
+            <a href={newsUrl} target='_blank' className='btn btn-sm btn-primary'>Read More</a>
+          </div>
+        </div>
       </div>
     )
   }
