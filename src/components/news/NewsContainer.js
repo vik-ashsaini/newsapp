f32d505f00472e671188d9d6c18ea0d8befa849f
vik-ashsaini <vikashsaini879@gmail.com>
2024-02-23 12:08:07 +0530

    previous and next button , pagination added

--- src/components/news/NewsContainer.js
+++ src/components/news/NewsContainer.js
@@ -1,41 +1,70 @@
-import React, { Component } from 'react';
-import NewsItem from './NewsItem';
-import { wait } from '@testing-library/user-event/dist/utils';
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class NewsContainer extends Component {
-  
-  constructor(){
+
+  constructor() {
     super();
-    this.state={
-      articles:[],
-      loading:false
-    }
+    this.state = {
+      articles: [],
+      loading: false,
+    };
   }
 
-  async componentDidMount()
-  {
-    let url='https://newsapi.org/v2/top-headlines?country=us&apiKey=dbe57b028aeb41e285a226a94865f7a7';
+  async componentDidMount() {
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=dbe57b028aeb41e285a226a94865f7a7&page=1&pagesize=10";
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({articles:parsedData.articles});
-    console.log(this.parsedData.articles);
+    this.setState({ articles: parsedData.articles,totalResults:parsedData.totalResults });
+    console.log(parsedData)
+  }
+
+  previousPage = async () =>{
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=dbe57b028aeb41e285a226a94865f7a7&page=${this.state.page-1}&pagesize=10`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({ 
+      articles: parsedData.articles,
+      page:this.state.page-1
+    });
+  }
+
+  nextPage = async () =>{
+    if(this.state.page+1>Math.ceil(this.state.totalResults/10))
+    {
+
+    }
+    else
+    {
+      let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=dbe57b028aeb41e285a226a94865f7a7&page=${this.state.page+1}&pagesize=10`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({ 
+        articles: parsedData.articles,
+        page:this.state.page+1
+      });
+    }    
   }
 
   render() {
     return (
-      <div className='container my-3'>
+      <div className="container my-3">
         <h2>News Monkey - Top Headlines</h2>
-        <div className='row'>
-        {this.state.articles.map((e)=>{
-          return <div className='col=md-4' key={e.url}>
-            <NewsItem title={e.title?e.title.slice(0,45):''} description={e.description?e.description.slice(0,88):''}
-            imageUrl={e.urlToImage} newsUrl={e.url} />
-          </div>
-        })}
+        <div className="row">
+          {this.state.articles.map((e) => {
+            return (<div className="col=md-4" key={e.url}>
+                <NewsItem title={e.title ? e.title.slice(0, 45) : ""} description={e.description ? e.description.slice(0, 88) : ""}
+                  imageUrl={e.urlToImage} newsUrl={e.url} />
+              </div>);
+          })}
+        </div>
+        <div className="row d-flex justify-content-between">
+          <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.previousPage}>&larr; Previous</button>
+          <button type="button" className="btn btn-dark" onClick={this.nextPage}>Next &rarr;</button>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsContainer
+export default NewsContainer;
--- src/components/news/NewsItem.js
+++ src/components/news/NewsItem.js
@@ -6,11 +6,11 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className='card'>
-          <img className='card-img-top' src={!imageUrl?imageUrl:''} alt=''/>
+          <img className='card-img-top' src={imageUrl?imageUrl:''} alt=''/>
           <div className='card-body'>
             <h5 className='card-title'>{title}...</h5>
             <p className='card-text'>{description}...</p>
-            <a href={newsUrl} target='_blank' className='btn btn-sm btn-primary'>Read More</a>
+            <a href={newsUrl} target='_blank' className='btn btn-sm btn-dark'>Read More</a>
           </div>
         </div>
       </div>
